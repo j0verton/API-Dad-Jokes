2f92b07ec7d6203509db58c6743ebedabb3aabfd
j0verton <overtonje@gmail.com>
2020-08-28 14:17:21 -0500

    completed ultra challenge

--- scripts/JokeProvider.js
+++ scripts/JokeProvider.js
@@ -4,13 +4,53 @@ let joke;
 //         Advanced: find a way to return a copy of the joke object (tip: .slice will not work)
 // 2. define and export a getJoke function that fetches a joke from the API and sets the joke variable to the response
 
-
 export const useJoke = (jokeObj) => {
-    return joke = Object.assign({}, jokeObj);
+    joke = Object.assign({}, jokeObj);
+    console.log("joke", joke)
+    return joke
+    // joke = jokeData[0]
+    // console.log("joke", joke)
 }
+
+// export const getJoke = () => {
+//     return fetch("https://official-joke-api.appspot.com/jokes/random")
+//     .then(response => response.json())   
+// } 
+
+
+// export const useJoke = (jokeObj) => {
+//     return joke = Object.assign({}, jokeObj);
+// }
+
+
 
 export const getJoke = (type) => {
-    debugger;
+// debugger;
     return fetch(`https://official-joke-api.appspot.com/jokes/${type}random`)
-    .then(response => response.json())   
+    .then(response => response.json())
 }
+
+
+
+
+
+
+
+// export const getJoke = (type) => {
+//     switch (type) {
+//         case "":
+//             return fetch("https://official-joke-api.appspot.com/jokes/random")
+//                 .then(response => response.json())    
+                
+//         case "general":
+//             return fetch("https://official-joke-api.appspot.com/jokes/general/random")
+//                 .then(response => response.json())
+                   
+//         case "knock-knock":
+//             return fetch("https://official-joke-api.appspot.com/knock-knock/random")
+//                 .then(response => response.json())
+//         case "programming":
+//             return fetch("https://official-joke-api.appspot.com/programming/random")
+//                 .then(response => response.json())
+//     }
+// }
--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,34 @@
+// import { useJoke, getJoke } from "./JokeProvider.js";
+// import {PunchLine, Joke } from "./Joke.js"
+
+// const jokeBtn = document.querySelector(".request-joke-btn");
+// const punchLineBtn = document.querySelector(".request-punch-btn");
+
+// let newJoke
+// let jokeType = document.querySelector("#type-selector").value;
+
+// jokeBtn.addEventListener("click", (e) => {
+//   console.log("I should be grabbing a joke now...")
+//   // TODO: fetch a joke from the Joke API and render it to the DOM
+  
+//   //using drop-down menu to select joke type 
+//   getJoke(jokeType)
+//   .then(useJoke())
+//   .then(responseData => {
+//   newJoke = responseData;
+//   let jokeHTML=Joke(newJoke);
+//   let jokeTarget = document.querySelector(".joke-setup")
+//   jokeTarget.innerHTML = jokeHTML;
+//   }); 
+// });
+
+// punchLineBtn.addEventListener("click", (e) => {
+//   let punchHTML=PunchLine(newJoke);
+//   let punchTarget = document.querySelector(".joke-punchline")
+//   punchTarget.innerHTML = punchHTML;
+// });
+
+
 import { useJoke, getJoke } from "./JokeProvider.js";
 import {PunchLine, Joke } from "./Joke.js"
 
@@ -7,17 +38,23 @@ const punchLineBtn = document.querySelector(".request-punch-btn");
 let newJoke
 
 jokeBtn.addEventListener("click", (e) => {
-  console.log("I should be grabbing a joke now...")
+  console.log("I should be grabbing a joke now...");
   // TODO: fetch a joke from the Joke API and render it to the DOM
-  
-  //using drop-down menu to select joke type 
   let jokeType = document.querySelector("#type-selector").value;
-  console.log(jokeType);
-
+  console.log(jokeType)
   getJoke(jokeType)
   .then(useJoke())
-  .then(responseData => {
-  newJoke = responseData;
+  .then(response => {
+  let jokeData = response;
+
+  //I had to insert this logic because when i specify a joke type the api returns 
+  //an array with the joke object in it, but if i dont specify it just returns an object
+  if (Array.isArray(jokeData)){
+    newJoke = jokeData[0]
+  } else {
+    newJoke = jokeData
+  }
+
   let jokeHTML=Joke(newJoke);
   let jokeTarget = document.querySelector(".joke-setup")
   jokeTarget.innerHTML =jokeHTML;
@@ -28,4 +65,4 @@ punchLineBtn.addEventListener("click", (e) => {
   let punchHTML=PunchLine(newJoke);
   let punchTarget = document.querySelector(".joke-punchline")
   punchTarget.innerHTML = punchHTML;
-});
+}); 
