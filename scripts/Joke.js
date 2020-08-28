8b1c5789f3161d50172eba2c5413f0b6037538c8
j0verton <overtonje@gmail.com>
2020-08-28 11:38:21 -0500

    got all initial challenges done

--- scripts/Joke.js
+++ scripts/Joke.js
@@ -1,2 +1,13 @@
 // Define and export a function called Joke.
 // It should accept a joke object as a parameter and return an html string
+export const Joke = (joke) => {
+    return `
+        <h2>${joke.setup}</h2>
+    `
+}  
+
+export const PunchLine = (joke) => {
+    return `
+        <h2>${joke.punchline}</h2>
+    `
+}  
--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,26 @@
+import { useJoke, getJoke } from "./JokeProvider.js";
+import {PunchLine, Joke } from "./Joke.js"
+
 const jokeBtn = document.querySelector(".request-joke-btn");
+const punchLineBtn = document.querySelector(".request-punch-btn");
+
+let newJoke
 
 jokeBtn.addEventListener("click", (e) => {
-  console.log("I should be grabbing a joke now...");
+  console.log("I should be grabbing a joke now...")
   // TODO: fetch a joke from the Joke API and render it to the DOM
+  getJoke()
+  .then(useJoke())
+  .then(responseData => {
+  newJoke = responseData;
+  let jokeHTML=Joke(newJoke);
+  let jokeTarget = document.querySelector(".joke-setup")
+  jokeTarget.innerHTML =jokeHTML;
+  }); 
+});
+
+punchLineBtn.addEventListener("click", (e) => {
+  let punchHTML=PunchLine(newJoke);
+  let punchTarget = document.querySelector(".joke-punchline")
+  punchTarget.innerHTML = punchHTML;
 });
