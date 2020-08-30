0ca94d4ab5b23e2028103225cdfa259a8d72f48e
j0verton <overtonje@gmail.com>
2020-08-30 11:29:16 -0500

    working on knockknock

--- scripts/Joke.js
+++ scripts/Joke.js
@@ -11,3 +11,20 @@ export const PunchLine = (joke) => {
         <h2>${joke.punchline}</h2>
     `
 }  
+ 
+export const KnockJoke = (joke) =>  {
+    let lineThreeOn = joke.setup.slice(28)
+    let lineThree = lineThreeOn.substring(0, lineThreeOn.indexOf('.'))
+    console.log(lineThreeOn, lineThree)
+    return `
+    <h2>Knock knock.</h2>
+    <button class="whos-there-btn">Who's there?</button>
+    <h2 class="line-three" style="display:none">${lineThree}.</h2>
+    <button class="who"  style="display:none">${lineThree} who?</button>
+
+`
+}
+
+// var s = '/Controller/Action?id=11112&value=4444';
+// s = s.substring(0, s.indexOf('?'));
+// document.write(s);
